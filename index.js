1465394a33e5bcf06c2d962800f56a53864ebc9d
CKillen <cl_killen@hotmail.com>
2019-05-16 15:18:33 -0400

    Finished animations, Added fontawesome social icons, Added most links

--- index.js
+++ index.js
@@ -2,15 +2,44 @@ const UP_ARROW_ID = 'up-arrow';
 const DOWN_ARROW_ID = 'down-arrow';
 const SELECTED_CLASS = 'selected';
 const ARROW_CLASS = 'arrow';
+const CONTACT_CLASS = 'contact';
 
-let allProjectsArray = Array.prototype.slice.call(document.querySelectorAll(".project"));
+let allProjectsArray = Array.prototype.slice.call(document.querySelectorAll('.project'));
 
 
 document.addEventListener('click', (clickEvent) => {
   if(clickEvent.target.classList.contains(ARROW_CLASS)) {
     arrowClickHandler(clickEvent.target);
   }
+  if(clickEvent.target.classList.contains(CONTACT_CLASS) || clickEvent.target.parentElement.classList.contains(CONTACT_CLASS)) {
+    contactAnimationHandler();
+  }
+
+  if(clickEvent.target.classList.contains('main-page-link') || clickEvent.target.parentElement.classList.contains('main-page-link')){
+    mainpageAnimationHandler();
+  }
 });
+
+function mainpageAnimationHandler() {
+  let contentElement = document.querySelector('.content');
+  let contactForm = document.querySelector('.contact-form')
+
+  contactForm.classList = 'contact-form contact-hide'
+
+  contentElement.classList = 'content show';
+
+
+
+}
+
+function contactAnimationHandler() {
+  let contentElement = document.querySelector('.content');
+  let contactForm = document.querySelector('.contact-form')
+
+  contentElement.classList = 'content hide';
+  contactForm.classList = 'contact-form contact-show';
+
+}
 
 function arrowClickHandler(clickedArrow) {
   if(clickedArrow.id === UP_ARROW_ID) {
@@ -51,11 +80,11 @@ function selectionChangedProjectsHandler(changedProjectsObject) {
     if(change != false) {
     //handle selected project
     //remove all classes but project! add last-selected
-    previousProject.classList = "project";
+    previousProject.classList = 'project';
     previousProject.classList.add('last-selected');
     //handle next selected project
     //remove all classes but project! Add either top-selected or bottom-selected
-    selectedProject.classList = "project";
+    selectedProject.classList = 'project';
     selectedProject.classList.add('selected');
     if(direction === 1) {
       selectedProject.classList.add('top-selected');
